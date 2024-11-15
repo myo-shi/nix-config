9b333bbbfcdeacc59d49ccca3f76969db0983372
myo <aokage000@gmail.com>
2024-11-15 20:07:29 +0900

    add nvim dotfiles

---
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -0,0 +1,50 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+if vim.g.vscode then
+  -- VSCode Neovim
+  local vscode = require("vscode")
+  local set = vim.keymap.set
+  local del = vim.keymap.del
+  local opts = { noremap = true, silent = true }
+  set({ "n" }, "<leader>a", function()
+    vscode.action("editor.action.quickFix")
+  end)
+  set({ "n" }, "<leader>e", function()
+    vscode.action("workbench.action.toggleSidebarVisibility")
+  end)
+  set({ "n" }, "gs", function()
+    vscode.action("typescript.goToSourceDefinition")
+  end)
+  set({ "n" }, "gr", function()
+    vscode.action("editor.action.goToReferences")
+  end)
+  del({ "n" }, "<S-h>")
+  set({ "n" }, "<S-h>", function()
+    vscode.action("workbench.action.previousEditor")
+  end)
+  del({ "n" }, "<S-l>")
+  set({ "n" }, "<S-l>", function()
+    vscode.action("workbench.action.nextEditor")
+  end)
+  set({ "n" }, "[e", function()
+    vscode.action("editor.action.marker.next")
+  end)
+  set({ "n" }, "]e", function()
+    vscode.action("editor.action.marker.prev")
+  end)
+  set({ "x", "i" }, "<C-d>", function()
+    vscode.with_insert(function()
+      vscode.action("editor.action.addSelectionToNextFindMatch")
+    end)
+  end)
+  set({ "i" }, "<C-a>", function()
+    vscode.action("editor.action.triggerSuggest")
+  end)
+  set({ "n" }, "<C-w>o", function()
+    vscode.action("workbench.action.closeOtherEditors")
+  end)
+  set({ "n" }, "<C-w><S-o>", function()
+    vscode.action("workbench.action.closeEditorsInOtherGroups")
+  end)
+end
+-- Add any additional keymaps here
--- home-manager/nvim.nix
+++ home-manager/nvim.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.neovim = {
     enable = true;
@@ -7,13 +7,19 @@
     package = pkgs.unstable.neovim-unwrapped;
   };
 
-  xdg.configFile.nvim = {
-    source = builtins.fetchGit {
-      url = "https://github.com/LazyVim/starter.git";
-      rev = "79b3f27f5cea8fe6bbb95ba04f93dffa545c5197";
-    };
+  xdg.configFile."nvim" = {
+    source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/workspace/nix-config/dotfiles/nvim";
     recursive = true;
   };
+
+  # xdg.configFile.nvim = {
+  #   source = builtins.fetchGit {
+  #     url = "https://github.com/LazyVim/starter.git";
+  #     rev = "79b3f27f5cea8fe6bbb95ba04f93dffa545c5197";
+  #   };
+  #   recursive = true;
+  # };
 
   programs.lazygit.enable = true;
 }
