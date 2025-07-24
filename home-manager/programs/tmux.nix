3a95d197461f1402590e6373d9b8ceb1a4a666d9
myo <myoshi.dev@gmail.com>
2025-07-24 21:57:45 +0900

    update tmux conf

--- home-manager/programs/tmux.nix
+++ home-manager/programs/tmux.nix
@@ -16,6 +16,8 @@
   programs.tmux = {
     enable = true;
     shell = "${pkgs.fish}/bin/fish";
+    terminal = "tmux-256color";
+    historyLimit = 100000;
     extraConfig = builtins.readFile ../../dotfiles/tmux.conf;
   };
 }
