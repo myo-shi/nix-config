ccf24c890376468145ccabf068cb88d000849368
myo <myoshi.dev@gmail.com>
2026-01-14 09:52:16 +0900

    vim spell check cjk setting

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -1,7 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
---
+
+vim.opt.spelllang = { "en", "cjk" }
+
 if vim.g.vscode then
   -- VSCode Neovim
   vim.opt.clipboard = { "unnamedplus" }
