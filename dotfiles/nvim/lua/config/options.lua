dd9411e57ca991a085bf95de1ae4a120ee0787ee
myo <myoshi.dev@gmail.com>
2026-01-31 23:24:13 +0900

    neovim clipboard osc52

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -3,6 +3,17 @@
 -- Add any additional options here
 
 vim.opt.spelllang = { "en", "cjk" }
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
 
 if vim.g.vscode then
   -- VSCode Neovim
